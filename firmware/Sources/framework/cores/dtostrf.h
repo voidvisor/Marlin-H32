6ebbedb09d394c9bd8e5fd701f2c76aa551ea767
Alex <alexqm@icloud.com>
2022-02-25 12:12:11 -0600

    Fix dtostrf

--- firmware/Sources/framework/cores/dtostrf.h
+++ firmware/Sources/framework/cores/dtostrf.h
@@ -1,9 +1,12 @@
 
+#define dtostrf fixed_dtostrf
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+// char *dtostrf (double val, signed char width, unsigned char prec, char *sout);
 
-char *dtostrf (double val, signed char width, unsigned char prec, char *sout);
+char *fixed_dtostrf(double __val, signed char __width, unsigned char __prec, char *__s);
 
 #ifdef __cplusplus
 }
